87f242e60e476701d496e1984fafe55d83bdf867
Srikanth Ganapavarapu <sganapavarapu@ebay.com>
2018-03-29 09:57:31 -0700

    In Data: (file: master_data.js) - changed 'functionSignature' property to be 'functionName' i.e. mere name & not the signature; so, signature needs to be constructed from the 'functionName' and 'parameters' In implementation: (file: oneProblem.html) - generating functionSignature from 'functionName' + 'parameters'.join() - replaced switch statement with ellipsis (...) / spread operator to auto insert function params from the 'parameters' property's array of param names - using 'use strict' during function construction - cosmetic changes to align "}" within <p> for the <textarea> etc.

--- class_work/codingbat_as_project/master_data.js
+++ class_work/codingbat_as_project/master_data.js
@@ -7,12 +7,12 @@ var w1Problems = {
 				
 				description: "The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.",
 
-				functionSignature: "function sleepIn(vacation, weekday)",
+				functionName: "sleepIn",
 
 				parameters: [ "vacation", "weekday" ],
 
 				testData: [
-					// each object here represents a row of a data instance for same test case
+					// each row represents a test case
 							{
 								parameters: [ true, true ], 
 								expectedResult: true
@@ -37,7 +37,7 @@ var w1Problems = {
 				
 				description: "We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.",
 
-				functionSignature: "function monkeyTrouble(aSmile, bSmile)",
+				functionName: "monkeyTrouble",
 
 				parameters: [ "aSmile", "bSmile" ],
 
