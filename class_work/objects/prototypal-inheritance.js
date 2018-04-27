8f402a3177fafecc3eba1845b0d7fcecf0cc249b
Srikanth Ganapavarapu <sganapavarapu@ebay.com>
2018-04-27 00:13:29 -0700

    fixed overriding and using parent prototype impl as part of overridden impl (super)

--- class_work/objects/prototypal-inheritance.js
+++ class_work/objects/prototypal-inheritance.js
@@ -3,35 +3,42 @@ function Person(name, dob, gender, address) {
 	this.dob = Date.parse(dob);
 	this.gender = gender;
 	this.address = address;
-	this.alive = true
-	this.jsonStringify = function() {
-			console.log("")
-			console.log(`Name: ${this.name}`);
-			console.log(`Age: ${this.age()}`);
-			console.log(`Gender: ${this.gender}`);
-			console.log(`Address: ${this.address}`);
-			(this.alive? console.log("") : console.log(`Expired: ${this.doe}`));
-		}
 }
 
 Person.prototype.age = function() {
-	var calculateAge = function() {
-		const timeNow = new Date()
-		const msDiff = timeNow - this.dob
-		return msDiff;
-	}
-	calculateAge = calculateAge.bind(this)
-	return calculateAge();
+	return ((new Date() - this.dob) + " ms");
 }
 
 Person.prototype.changeAddress = function(newAddress) {
 	this.address = newAddress;
 }
 
- Person.prototype.expired = function(doe) {
+Person.prototype.expired = function(doe) {
 	this.doe = Date.parse(doe)
-	this.alive = false
 }
+
+Person.prototype.isAlive = function() {
+	return !this.doe;
+}
+
+Person.prototype.toString = function() {
+	var str = (`\t\tName: ${this.name}`);
+	str += (`\n\t\tAge: ${this.age()}`);
+	str += (`\n\t\tGender: ${this.gender}`);
+	str += (`\n\t\tAddress: ${this.address}`);
+	(this.alive) && (str += (`\n\t\tExpired: ${this.doe}`));
+	return str;
+}
+
+
+var p = new Person("Mother Teresa", "1970-01-01", "F", "Calcutta")
+p.changeAddress("Kolkata")
+p.expired("1999-12-22")
+
+console.log(`\np.toString(): [\n${p.toString()}\n\t    ]`)
+console.log(`typeof p: ${typeof p}`)
+console.log(`p instanceof <Person>: ${p instanceof Person}`)
+console.log(`p instanceof <Patient>: ${p instanceof Patient}`)
 
 
 // Prototype Inheritance
@@ -39,20 +46,12 @@ Person.prototype.changeAddress = function(newAddress) {
 // 2. create shallow copy of parent prototype in child prototype
 // 3. add extra methods that belong in child prototype only
 
+// constructor
 function Patient(name, dob, gender, address, bloodGroup, ailments, vitals) {
 	Person.call(this, name, dob, gender, address);
 	this.bloodGroup = bloodGroup;
 	this.ailments = ailments
 	this.vitals = vitals
-
-	this.myStringify = this.jsonStringify.bind(this);
-
-	this.jsonStringify = function() {
-			this.myStringify();
-			console.log(`Blood Group: ${this.bloodGroup}`);
-			console.log(`Ailments: ${JSON.stringify(this.ailments)}`);
-			console.log(`Vitals: ${JSON.stringify(this.vitals)}`);
-	}
 }
 
 // WRONG: Patient.prototype = Person.prototype
@@ -60,20 +59,20 @@ function Patient(name, dob, gender, address, bloodGroup, ailments, vitals) {
 
 Patient.prototype = Object.assign({}, Person.prototype)
 
-var p = new Person("Mother Teresa", "1970-01-01", "F", "Calcutta")
-p.changeAddress("Kolkata")
-p.expired("1999-12-22")
-p.jsonStringify();
-console.log(`typeof p: ${typeof p}`)
-console.log(`p instanceof Person: ${p instanceof Person}`)
-console.log(`p instanceof Patient: ${p instanceof Patient}`)
+Patient.prototype.toString = function() {
+	var str = Person.prototype.toString.call(this);
+	str += (`\n\t\tBlood Group: ${this.bloodGroup}`);
+	str += (`\n\t\tAilments: ${JSON.stringify(this.ailments)}`);
+	str += (`\n\t\tVitals: ${JSON.stringify(this.vitals)}`);
+	return str;
+}
 
-var k = new Patient("Donald Trump", "1970-01-01", "M", "Some Place", 
+var k = new Patient("Donald Duck", "1970-01-01", "M", "Some Place", 
 					"COLA", ["demented"], ["5'7''", "220lb"])
-k.changeAddress("WhiteHouse")
-k.jsonStringify();
-console.log(`typeof k: ${typeof k}`)
-console.log(`k instanceof Person: ${k instanceof Person}`)
-console.log(`k instanceof Patient: ${k instanceof Patient}`)
+k.changeAddress("BlueHouse")
 
+console.log(`\nk.toString(): [\n${k.toString()}\n\t    ]`)
+console.log(`typeof k: ${typeof k}`)
+console.log(`k instanceof <Person>: ${k instanceof Person}`)
+console.log(`k instanceof <Patient>: ${k instanceof Patient}`)
 
